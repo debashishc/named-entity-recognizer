e0dc6dd050fd127255b334f735fb84f55e79f5c0
DCANU <dc@Debashishs-MacBook-Pro.local>
2016-10-04 14:39:49 +1100

    Project imported to Intellij

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,11 @@
 nano conll-eval.pl
 
 # Train
+# Usage: crf_learn -f 3 -c 1.5 template-file train_file model_file
 crf_learn examples/template-example conll2002/esp.train modleExample
 
 # Test
+# Usage: crf_test -m model_file test_files ...
 crf_test -m modleExample conll2002/esp.testa
 
 # 5
