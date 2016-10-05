c28540a7f3b075723d87fcfc5899aad9542c4412
DCANU <dc@Debashishs-MacBook-Pro.local>
2016-10-05 14:52:09 +1100

    Possibly finished TestLabeler, added a test.txt

--- NLPTools/src/nlp/opennlp/TestLabeler.java
+++ NLPTools/src/nlp/opennlp/TestLabeler.java
@@ -28,7 +28,7 @@ public class TestLabeler {
     public static POSTagger.POSTagging tagText(String text) throws IOException {
 
         POSTagger posTagger = new POSTagger();
-        POSTagger.POSTagging posTagging = posTagger.process(text, 1);
+        POSTagger.POSTagging posTagging = posTagger.process(text, 3);
 
         return posTagging;
     }
@@ -40,29 +40,32 @@ public class TestLabeler {
         String[/*sent*/][/*word*/] _tokens = posTagging._tokens;
         String[/*sent*/][/*tag*/][/*word*/] _taggings = posTagging._taggings;
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile, false));
 
             for (int si = 0; si < _taggings.length; si++) {
-                bufferedWriter.write("Sentence #" + si + " [" + _tokens[si].length + "]: ");
                 for (int ti = 0; ti < _taggings[si].length; ti++) {
-                    bufferedWriter.write("\n- Tagging #" + ti + ": ");
                     for (int wi = 0; wi < _taggings[si][ti].length; wi++) {
-                        bufferedWriter.write(_tokens[si][wi] + "/");
-                        bufferedWriter.write(_taggings[si][ti][wi] + " ");
+                        bufferedWriter.write(_tokens[si][wi] + "\t");
+                        bufferedWriter.write(_taggings[si][ti][wi] + "\t" + "O" + "\n");
                     }
                 }
-                bufferedWriter.write("\n\n");
-                bufferedWriter.close();
+                bufferedWriter.write("\n");
             }
-        }
+
+        bufferedWriter.close();
+
     }
 
 
 
     public static void main(String[] args) throws IOException {
 
-        String inputPath = "test.txt";
-        String outputPath = "labeled_test.txt";
+        String inputPath = "./test.txt";
+        // todo: change it to ".test" file before testing later
+        String outputPath = "./labeled_test.txt";
+
+//        System.out.println(new File(".").getCanonicalPath());
+
 
         String body = readFile(inputPath);
         POSTagger.POSTagging posTagging = tagText(body);
