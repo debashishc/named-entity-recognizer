8615c2c2414d1183e258b805b48806f2850cf9c7
DCANU <dc@Debashishs-MBP.lan>
2016-10-08 00:20:33 +1100

    Created a template with required f1 score

--- cmd.sh
+++ cmd.sh
@@ -2,12 +2,12 @@ nano conll-eval.pl
 
 # Train
 # Usage: crf_learn -f 3 -c 1.5 template-file train_file model_file
-crf_learn examples/template-example conll2002/esp.train model_file
+crf_learn template_files/template conll2002/esp.train model_file
 
 # Test
 # Usage: crf_test -m model_file test_files ...
-crf_test -m modleExample conll2002/esp.testa
+crf_test -m model_file conll2002/esp.testa
 
 # 5
-crf_test -m modleExample conll2002/esp.testa > predicted.txt
+crf_test -m model_file conll2002/esp.testa > predicted.txt
 perl conll-eval.pl -d "\t" < predicted.txt
